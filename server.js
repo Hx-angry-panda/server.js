5a26146ab116bb896a7ec5a89bbe6136ddce8dcb
Hua <924193579@qq.com>
2019-08-03 17:23:10 +0800

    更新

--- server.js
+++ server.js
@@ -21,26 +21,15 @@ var server = http.createServer(function(request, response){
 
   console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-  if(path === '/style.css'){
-      response.setHeader('Content-Type','text/css; charset=utf-8')
-      response.write('body{background-color: #ddd;}h1{color: red;}')
-      response.end()  
-  }else if(path === '/'){
-      response.setHeader('Content-Type','text/html; charset=utf-8')
-      response.write('<!DOCTYPE>\n<html>' + 
-      '<head><link rel="stylesheet" href="/style.css">' +
-      '</head><body>' +
-      '<h1>Hello Server</h1>' +
-      '<script src="/main.js"></script>' +
-      '</body></html>')
-      response.end()
-  }else if(path == '/main.js'){
-    response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-    response.write('alert("这是JS执行的")')
+  if(path === '/'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('哈哈哈')
     response.end()
-    }
-  else{
+  }else{
     response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('呜呜呜')
     response.end()
   }
 
@@ -49,3 +38,5 @@ var server = http.createServer(function(request, response){
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
+
